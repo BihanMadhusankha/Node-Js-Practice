3c0489a8a653e12ca4847e29f0bcc284dbbf205e
BihanMadhusankha <ellawalagemadhusankha02@gmail.com>
2024-04-07 09:30:45 +0530

    create CRUD operation

---
+++ express-project/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express();
+const {errorHandler} = require('./middleware/errorHandle');
+const connectDB = require('./config/dbConnection');
+
+connectDB();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/api/contacts",require('./routes/contactRoutes'));
+app.use(errorHandler);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
